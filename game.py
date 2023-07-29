704c109dd43e9f3534133bff1878e0b28fc31395
Carusi <giuseppe.carusi@capgemini.com>
2023-07-29 21:02:52 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,32 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
+        self.display_surface = pygame.display.get_surface()
+        self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+
+        # lines
+        self.lines_surface = self.surface.copy()
+        self.lines_surface.fill((0,255,0))
+        self.lines_surface.set_colorkey((0,255,0))
+        self.lines_surface.set_alpha(120)
+
+    def draw_grid(self):
+        for col in range(1, COLUMNS):
+            x = col * CELL_SIZE
+            pygame.draw.line(self.lines_surface, LINE_COLOR, (x,0),(x,self.surface.get_height()), 1)
+        
+        for row in range(1, ROWS):
+            y = row * CELL_SIZE
+            pygame.draw.line(self.lines_surface, LINE_COLOR, (0, y), (self.surface.get_width(),y))
+
+        self.surface.blit(self.lines_surface, (0,0))
+
+    def run(self):
+
+        self.surface.fill(GRAY)
+        self.draw_grid()
+        self.display_surface.blit(self.surface, (PADDING, PADDING))
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+        
